0c9bdcff477bb9ad355e528b5a3ea52629688196
efea <astarlioglu@outlook.com>
2023-03-17 01:07:49 +0200

    added 2_11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from "./components/Persons"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   //const [newName, setName] = useState('')
   //const [newNumber, setNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -45,16 +41,18 @@ const App = () => {
     setPersonList(persons.filter(person => person.name.toLowerCase().includes(filtStr)))
   }
 
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersonList(response.data)
+        setPersons(response.data);
+      })
+  }, [])
 
-/*
-  const NameChangeHandler = (event) => {
-    setName(event.target.value);
-  }
-
-  const NumberChangeHandler = (event) => {
-    setNumber(event.target.value)
-  }
-*/
+  console.log("render", personList.length , "persons");
   return (
     <div>
       <h2>Phonebook</h2>
