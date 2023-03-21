131a43f417bec81aa8cc4916f729d9304b0be9fd
efea <astarlioglu@outlook.com>
2023-03-21 23:18:01 +0200

    making frontend work with deployed backend

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const personsURL = "http://localhost:3001/persons"
+const personsURL = '/api/persons'
 const getAll = () => {
   const request = axios.get(personsURL);
   return request.then((response) => response.data)
